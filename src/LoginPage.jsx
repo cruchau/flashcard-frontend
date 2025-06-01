dea02db5799fbbbd76a2effbbc0f39a0f37d0ba5
cruchau <arnaud.cruchaudet@outlook.fr>
2025-06-01 11:42:20 +0100

    Create LoginPage.jsx

---
+++ src/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Page de Connexion</h1>
+      <p>Contenu de la page login ici...</p>
+    </div>
+  );
+}
